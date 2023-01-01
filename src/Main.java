8e4ebd02d0414b5f56a0d9bc20acfcd267b6fe2f
Elizaveta Mishina <test.lizaveta@gmail.com>
2023-01-01 19:53:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 3000;
+        int mile = 20;
+
+        int bonus = price / mile;
+
+
+        System.out.print("Итоговые мили:" + bonus);
+
+    }
+}
